3a6617c8600df24a4c6ac790c512620cfe3bbb81
Huu Ty <typrone1@gmail.com>
2017-09-06 10:25:50 +0700

    dua giao dien vao laravel

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\HoToc;
+use App\HoSo;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -13,5 +14,132 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $hoToc = HoToc::create(['tenhotoc'=>'Tộc Họ Nguyễn']);
+        $hoSo = HoSo::create([
+            'mahotoc' => 1,
+            'mahsbo' => null,
+            'mahsme' => null,
+            'hoten' => 'Nguyễn Văn Trưởng',
+            'ngaysinh' => '1950-08-20',
+            'gioitinh' => true,
+            'doithu' => 1,
+            'conthu' => 1,
+            'quequan' => 'Lorem Ipsum',
+            'diachi' => 'Lorem Ipsum',
+            'nguoiky' => 'Ông A',
+            'damat' => true
+        ]);
+        $hoSo = HoSo::create([
+            'mahotoc' => 1,
+            'mahsbo' => null,
+            'mahsme' => null,
+            'hoten' => 'Nguyễn Văn Trưởng',
+            'ngaysinh' => '1950-08-20',
+            'gioitinh' => true,
+            'doithu' => 1,
+            'conthu' => 1,
+            'quequan' => 'Lorem Ipsum',
+            'diachi' => 'Lorem Ipsum',
+            'nguoiky' => 'Ông A',
+            'damat' => true
+        ]);
+        $hoSo = HoSo::create([
+            'mahotoc' => 1,
+            'mahsbo' => null,
+            'mahsme' => null,
+            'hoten' => 'Nguyễn Văn Trưởng',
+            'ngaysinh' => '1950-08-20',
+            'gioitinh' => true,
+            'doithu' => 1,
+            'conthu' => 1,
+            'quequan' => 'Lorem Ipsum',
+            'diachi' => 'Lorem Ipsum',
+            'nguoiky' => 'Ông A',
+            'damat' => true
+        ]);
+        $hoSo = HoSo::create([
+            'mahotoc' => 1,
+            'mahsbo' => null,
+            'mahsme' => null,
+            'hoten' => 'Nguyễn Văn Trưởng',
+            'ngaysinh' => '1950-08-20',
+            'gioitinh' => true,
+            'doithu' => 1,
+            'conthu' => 1,
+            'quequan' => 'Lorem Ipsum',
+            'diachi' => 'Lorem Ipsum',
+            'nguoiky' => 'Ông A',
+            'damat' => true
+        ]);
+        $hoSo = HoSo::create([
+            'mahotoc' => 1,
+            'mahsbo' => null,
+            'mahsme' => null,
+            'hoten' => 'Nguyễn Văn Trưởng',
+            'ngaysinh' => '1950-08-20',
+            'gioitinh' => true,
+            'doithu' => 1,
+            'conthu' => 1,
+            'quequan' => 'Lorem Ipsum',
+            'diachi' => 'Lorem Ipsum',
+            'nguoiky' => 'Ông A',
+            'damat' => true
+        ]);
+        $hoSo = HoSo::create([
+            'mahotoc' => 1,
+            'mahsbo' => null,
+            'mahsme' => null,
+            'hoten' => 'Nguyễn Văn Trưởng',
+            'ngaysinh' => '1950-08-20',
+            'gioitinh' => true,
+            'doithu' => 1,
+            'conthu' => 1,
+            'quequan' => 'Lorem Ipsum',
+            'diachi' => 'Lorem Ipsum',
+            'nguoiky' => 'Ông A',
+            'damat' => true
+        ]);
+        $hoSo = HoSo::create([
+            'mahotoc' => 1,
+            'mahsbo' => null,
+            'mahsme' => null,
+            'hoten' => 'Nguyễn Văn Trưởng',
+            'ngaysinh' => '1950-08-20',
+            'gioitinh' => true,
+            'doithu' => 1,
+            'conthu' => 1,
+            'quequan' => 'Lorem Ipsum',
+            'diachi' => 'Lorem Ipsum',
+            'nguoiky' => 'Ông A',
+            'damat' => true
+        ]);
+        $hoSo = HoSo::create([
+            'mahotoc' => 1,
+            'mahsbo' => null,
+            'mahsme' => null,
+            'hoten' => 'Nguyễn Văn Trưởng',
+            'ngaysinh' => '1950-08-20',
+            'gioitinh' => true,
+            'doithu' => 1,
+            'conthu' => 1,
+            'quequan' => 'Lorem Ipsum',
+            'diachi' => 'Lorem Ipsum',
+            'nguoiky' => 'Ông A',
+            'damat' => true
+        ]);
+        $hoSo = HoSo::create([
+            'mahotoc' => 1,
+            'mahsbo' => null,
+            'mahsme' => null,
+            'hoten' => 'Nguyễn Văn Trưởng',
+            'ngaysinh' => '1950-08-20',
+            'gioitinh' => true,
+            'doithu' => 1,
+            'conthu' => 1,
+            'quequan' => 'Lorem Ipsum',
+            'diachi' => 'Lorem Ipsum',
+            'nguoiky' => 'Ông A',
+            'damat' => true
+        ]);
+
     }
 }
